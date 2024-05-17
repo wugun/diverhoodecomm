1b9664dc69caf5e04c0efeb3787556d80ebf50d2
Aiden Liu <wugunawki@gmail.com>
2024-05-16 18:03:41 -0700

    update href link for product page

--- components/Product.jsx
+++ components/Product.jsx
@@ -6,7 +6,7 @@ import { urlFor } from '@/pages';
 const Product = ({product: {image, name, slug, price}}) => {
   return (
     <div>
-      <Link href={`/product/${slug.current}`}>
+      <Link href={`/product/${slug.current ?? ''}`}>
         <div className="product-card">
             <img src={urlFor(image && image[0])}
             width={250}
